93cfb3080e2d185e16ea29080db53c88fc72d223
Khoa Dang <khoa9205141@gmail.com>
2022-10-08 15:47:35 +0700

    Footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Header from "./components/header";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from "components/footer";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Header />
-        <Routes></Routes>
+        <Footer />
       </div>
     </BrowserRouter>
   );
