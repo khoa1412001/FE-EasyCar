120c4677d3b1140acfcc67ea669304c1f27a326e
Khoa Dang <khoa9205141@gmail.com>
2022-10-19 18:40:55 +0700

    route

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState } from "react";
 import VehicleSignupPage from "pages/VehicleSignupPage";
+import ConfigRoute from "ConfigRoute";
 
 function App() {
   const [openSignin, setOpenSignin] = useState(false)
   return (
     <BrowserRouter>
-      <div className="App">
-      <VehicleSignupPage/>
-      </div>
+      <ConfigRoute/>
     </BrowserRouter>
   );
 }
